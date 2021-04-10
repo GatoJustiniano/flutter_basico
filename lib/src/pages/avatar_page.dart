86a720b7c73d9263190ae95c4484a5320a82eebe
Andres Justiniano <andres95liverpool@gmail.com>
2021-04-10 00:22:55 -0400

    Add Animación de container con flujo curvo

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Página de Avatar'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://static.wikia.nocookie.net/riverdale/images/b/b8/Lili_Reinhart.png/revision/latest/scale-to-width-down/350?cb=20190711215453&path-prefix=es'),
+              radius: 30.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 15.0),
+            child: CircleAvatar(
+              child: Text('AJ'),
+              backgroundColor: Colors.orange,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'), 
+          image: NetworkImage('https://vader.news/__export/1611097172597/sites/gadgets/img/2021/01/19/lili-reinhart-de-riverdale-bisexuak.jpg_256019641.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base_inicial/src/pages/alert_page.dart';
+import 'package:flutter_base_inicial/src/pages/animated_container.dart';
 import 'package:flutter_base_inicial/src/pages/avatar_page.dart';
 import 'package:flutter_base_inicial/src/pages/card_page.dart';
 import 'package:flutter_base_inicial/src/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 return <String, WidgetBuilder>{
-    '/'           : (BuildContext context) => HomePage(),
-    'alert'       : (BuildContext context) => AlertPage(),
-    'avatar'      : (BuildContext context) => AvatarPage(),
-    'card'        : (BuildContext context) => CardPage(),
+    '/'                   : (BuildContext context) => HomePage(),
+    'alert'                 : (BuildContext context) => AlertPage(),
+    'avatar'                : (BuildContext context) => AvatarPage(),
+    'card'                  : (BuildContext context) => CardPage(),
+    'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
   };
 }
